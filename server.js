e12c9782b41ca4a2ad156fd11bb9aece75b8aee2
Kurama <renderosalexis17@gmail.com>
2019-01-20 14:52:37 -0600

    It works now!!! Hooray! -Alexis

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 const port = 3000
 const express = require('express')
 const bodyParser = require('body-parser');
+const request = require('request');
+const fs = require('fs') 
 const app = express()
+var apiKey = null
+  
+fs.readFile('apiKey.txt', (err, data) => { 
+  	apiKey = data.toString();
+  	console.log(apiKey)
+})
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-	res.render('index');
+	res.render('index', {weather: null, error: null});
 })
 
 app.post('/', function (req, res) {
-	console.log(req.body.city);
-	res.render('index');
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's currently ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
